28026254aa0de9dc67c1776461e553b8bcccd584
KMatveychikov <matv00@gmail.com>
2020-08-25 19:28:22 +0300

    add homework v2

--- src/homework.java
+++ src/homework.java
@@ -6,26 +6,45 @@ public class homework {
     public static void main(String[] args){
         java.util.Scanner scanner = new Scanner(System.in);
 
-        int number = (int)(Math.random()*10);
-        int a = 0;
-        while(a<3){
-            System.out.println("Угадайте число от 0 до 9");
+        int number = (int)(Math.random()*10); //загадываем число
+        int a = 0; //попытки
+        int ask = 1;
+        while (ask == 1) {
+            while (true) {
 
-            int input_number = scanner.nextInt();
-            if(input_number==number){
-                System.out.println("Вы угадали");
-                break;
-            } else if (input_number>number){
-                System.out.println("Загаданное число меньше");
-                a++;
-            } else if(input_number<number) {
-                System.out.println("Загаданное число больше");
-                a++;
+                if (a == 3) {
+                    System.out.println("Попытки кончились:(");
+                    break;
+                }
+
+                System.out.println("Угадайте число от 0 до 9");
+
+
+
+                int input_number = scanner.nextInt(); //слушаем ответ в консоли
+
+                if (input_number == number) {
+                    System.out.println("Вы угадали");
+                    break;
+                } else if (input_number > number) {
+                    System.out.println("Загаданное число меньше");
+                    a++;
+                } else if (input_number < number) {
+                    System.out.println("Загаданное число больше");
+                    a++;
+                }
             }
-
+            System.out.println("еще раз? 1 - да, 0 - нет");
+            ask = scanner.nextInt();
+            a = 0;
 
         }
-        System.out.println("Попытки кончились:(");
     }
 
-}
+
+          }
+
+
+
+
+
